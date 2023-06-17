3bd6210ca8787bc57e2d5c3527c4e37bbfd4feff
aaditya010202 <aadibhatnagar535@gmail.com>
2023-06-17 16:18:20 +0530

    stocks buy and sell leetcode

---
+++ Arrays/Medium/stocksLeetcode.cpp
@@ -0,0 +1,18 @@
+// Leetcode
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int mini=prices[0];
+        int ans=0,cost;
+
+        for(int i=1;i<prices.size();i++)
+        {
+            cost=prices[i]-mini;
+            ans=max(cost,ans);
+            mini=min(mini,prices[i]);
+        }
+        
+        return ans;
+    }
+};
