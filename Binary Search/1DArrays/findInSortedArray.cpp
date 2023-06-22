871c199404c0a400425d81f1e17766c3f1c1d199
aaditya010202 <aadibhatnagar535@gmail.com>
2023-06-22 13:28:16 +0530

    find x in sorted array using binary search

---
+++ Binary Search/1DArrays/findInSortedArray.cpp
@@ -0,0 +1,22 @@
+// leetcode
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low<high)
+        {
+            int mid=low+(high-low+1)/2;
+            if(nums[mid]>target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid;
+            }
+        }
+        return nums[low]==target?low:-1;
+    }
+};
