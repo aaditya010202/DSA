9e45e896eac61682d21a3a2d41a4641833afd9b4
aaditya010202 <aadibhatnagar535@gmail.com>
2023-06-25 22:32:47 +0530

    find the ceil and the floor of the unsorted array

---
+++ Binary Search/1DArrays/CeilTheFloor.cpp
@@ -0,0 +1,41 @@
+// GFG
+
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+    // code here
+    sort(arr,arr+n);
+    pair<int,int> p;
+    
+    int low=0;
+    int high=n-1;
+    int floor=-1;
+    int ceil=-1;
+    while(low<=high)
+    {
+        int mid=((low+high)/2);
+        if(arr[mid]<=x){
+            floor=arr[mid];
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+        
+    }
+    low=0;
+    high=n-1;
+    while(low<=high)
+    {
+        int mid=((low+high)/2);
+        if(arr[mid]>=x)
+        {
+            ceil=arr[mid];
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    p=make_pair(floor,ceil);
+    return p;
+}
