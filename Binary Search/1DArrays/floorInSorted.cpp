ec167999a59f90bbafd0016773e7a311052c16fa
aaditya010202 <aadibhatnagar535@gmail.com>
2023-06-25 22:10:44 +0530

    find the floor in sorted array

---
+++ Binary Search/1DArrays/floorInSorted.cpp
@@ -0,0 +1,28 @@
+// GFG
+
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+       int low=0;
+       int high=n-1;
+       int ans=-1;
+       while(low<=high)
+       {
+           int mid=((low+high)/2);
+           if(v[mid]<=x){
+               ans=mid;
+               low=mid+=1;
+           }
+           else{
+               high=mid-1;
+           }
+       }
+       return ans;
+        
+    }
+};
