79ef67bd169f0f5a996cda712d8b67cf1a879bee
aaditya010202 <aadibhatnagar535@gmail.com>
2023-07-09 16:01:57 +0530

    reverse recursion

--- LinkedList/MediumLL/reverseLL.cpp
+++ LinkedList/MediumLL/reverseLL.cpp
@@ -1,5 +1,3 @@
-// leetcode
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,17 +11,32 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        // Node* temp=head;
+
+        // ITERATIVE
+
+        // if(head==NULL || head->next==NULL) return head;
+        // ListNode* front=NULL;
+        // ListNode* temp=NULL;
+        // while(head!=NULL)
+        // {
+        //     temp=head;
+        //     head=head->next;
+        //     temp->next=front;
+        //     front=temp;
+        // }
+        // return temp;
+
+
+        // RECURSIVE
+
         if(head==NULL || head->next==NULL) return head;
-        ListNode* front=NULL;
-        ListNode* temp=NULL;
-        while(head!=NULL)
-        {
-            temp=head;
-            head=head->next;
-            temp->next=front;
-            front=temp;
-        }
-        return temp;
+        // ListNode* temp1=NULL;
+        ListNode* temp2= reverseList(head->next);
+        ListNode* headNext=head->next;
+        headNext->next=head;
+        head->next=NULL;
+        return temp2;
+
+        // go till the end of list and take the last node as the new head of the list now return this head in the variable temp2. Now the head points to the second last element in the list and the head->next will be the last element which is also the temp2, so now head->next->next should be equal to head(second last element) to reverse the 2 nodes and in the end we return the last node everytime which is temp2
     }
 };
