51fda2d32f8c31d03ea0b4fbd66d39129ebf048f
aaditya010202 <aadibhatnagar535@gmail.com>
2023-08-03 15:52:27 +0530

    min path sum

---
+++ DP/2D_3DandGrids/MinPathSum.cpp
@@ -0,0 +1,91 @@
+// leetcode
+class Solution {
+
+
+//     private:
+//     int f(vector<vector<int>>& grid, int i, int j)
+//     {
+//         if(i==0&&j==0) return grid[i][j];
+//         if(i<0||j<0) return INT_MAX;
+
+//         int left=grid[i][j]+f(grid,i,j-1);
+//         int up=grid[i][j]+f(grid,i-1,j);
+
+//         return min(left,up);
+//     }
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         return f(grid,grid[0].size()-1,grid.size()-1);
+//     }
+
+
+// NEW
+//RECURSION
+// private:
+//     int solve(vector<vector<int>> &grid, int row, int col)
+//     {
+//         if(row==0 && col==0) return grid[row][col];
+//         if(row<0 || col<0) return INT_MAX;
+//         int up=solve(grid, row-1, col);
+//         int left=solve(grid, row, col-1);
+
+//         return grid[row][col]+min(up,left);
+//     }
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int r=grid.size();
+//         int c=grid[0].size();
+//         return solve(grid, r-1 , c-1);
+//     }
+
+
+// MEMOIZATION
+// private:
+//     int solve(vector<vector<int>> &grid, int row, int col, vector<vector<int>> &dp)
+//     {
+//         if(row==0 && col==0) return grid[row][col];
+//         if(row<0 || col<0) return INT_MAX;
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         int up=solve(grid, row-1, col,dp);
+//         int left=solve(grid, row, col-1,dp);
+
+//         return dp[row][col]=grid[row][col]+min(up,left);
+//     }
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int r=grid.size();
+//         int c=grid[0].size();
+//         vector<vector<int>> dp(r,vector<int> (c,-1));
+//         // dp[0][0]=grid[0][0];
+        
+//         return solve(grid,r-1,c-1,dp);
+//     }
+
+
+// TABULATION
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int r=grid.size();
+        int c=grid[0].size();
+        vector<vector<int>> dp(r,vector<int> (c,-1));       
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(i==0 && j==0)
+                {
+                    dp[i][j]=grid[i][j];
+                }
+                else{
+                    int up,left;
+                    if(i>0) up=grid[i][j]+dp[i-1][j];
+                    else up=INT_MAX;
+                    if(j>0) left=grid[i][j]+dp[i][j-1];
+                    else left=INT_MAX;
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[r-1][c-1];
+    }
+};
