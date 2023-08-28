4bc9b8cc2e512cd76d5082d4d224804d4044963f
aaditya010202 <aadibhatnagar535@gmail.com>
2023-08-28 19:54:58 +0530

    topological sorting

---
+++ Graphs/TopoSortAndProblems/TopoSort.cpp
@@ -0,0 +1,41 @@
+// coding ninjas
+#include<bits/stdc++.h>
+vector<int> topologicalSort(vector<vector<int>> &graph, int edges, int nodes) {
+    // Write your code here!
+    vector<vector<int>> arr(nodes);
+    vector<int> indegree(nodes,0);
+    for(auto x: graph)
+    {
+        arr[x[0]].push_back(x[1]);
+        indegree[x[1]]++;
+    }
+
+    queue<int> q;
+    vector<int> ans;
+    for(int i=0;i<nodes;i++)
+    {
+        if (indegree[i] == 0) {
+          q.push(i);
+        }
+    }
+
+    vector<int> vis(nodes,0);
+    while(!q.empty())
+    {
+        int front=q.front();
+        vis[front]=1;
+        ans.push_back(front);
+        q.pop();
+        for(auto x: arr[front])
+        {
+            indegree[x]--;
+            if(!vis[x] && indegree[x]==0)
+            {
+                q.push(x);
+            }
+        }
+    }
+    return ans;
+
+}
+
